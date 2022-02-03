773d6a7cb284e3bc88487699baebb18075bd3b92
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2022-02-03 11:46:22 +1100

    adds function to provide design temps

--- weather.py
+++ weather.py
@@ -4,21 +4,27 @@ import matplotlib.pyplot as plt
 from meteostat import Point, Daily, Monthly
 from calendar import monthrange
 import numpy as np
-import pandas as pd
-# from astral import LocationInfo
-from suntime import Sun
 
-def get_sun_times(location_coords, ymd):
-    # s = sun(city.observer, date=datetime.date(ymd[0], ymd[1], ymd[2]))
-    # city = LocationInfo(51.5, -0.116)
-    sun = Sun(location_coords[0], location_coords[1])
-    day = date(ymd[0], ymd[1], ymd[2])
-    sunrise = sun.get_local_sunrise_time(day).time()
-    sunset = sun.get_local_sunset_time(day).time()
-    sunrise = datetime(ymd[0], ymd[1], ymd[2],sunrise.hour, sunrise.minute)
-    sunset = datetime(ymd[0], ymd[1], ymd[2], sunset.hour, sunset.minute)
-    noon = sunrise + (sunset-sunrise)/2
-    return sunrise, sunset, noon
+
+def location_design_temp(location_coords, elevation, num_years=10, percentiles=[1, 99]):
+    """
+    Determines the design day max and min temperatures for a location by looking at num_years
+    historical daily temperature data. For the min temperature we use the percentiles[0] value
+    of the daily mins, i.e. by default this excludes 1% of the lowest temperatures. For the max we
+    use percentiles[1] value, which by default excludes 1% of the hottest days.
+    :param location_coords: (N, E) gps coords
+    :param elevation: float value for elevation at location (m)
+    :param num_years: (default=10) number of years of daily temperature data to consider
+    :param percentiles: (default=[1,99]) the upper and lower percentiles for temperatures to consider
+    :return: min_temp, max_temp, avg_temp
+    """
+    start = [2021-num_years, 1, 1]
+    end = [2021, 1, 1]
+    data = historical_daily_temperatures(start, end, location_coords, elevation)
+    min_tmp = np.percentile(data.tmin, percentiles[0])
+    max_tmp = np.percentile(data.tmax, percentiles[1])
+    avg_tmp = np.mean(data.tavg)
+    return min_tmp, max_tmp, avg_tmp
 
 def historical_daily_temperatures(start, end, location_coords, elevation):
     # start as list [year, month, day]
@@ -53,24 +59,24 @@ def typical_months_temperatures(month, location_coords, elevation):
     return tavg.mean(axis=0), tmin.mean(axis=0), tmax.mean(axis=0)
 
 
-
 if __name__=='__main__':
-    start = [2020,1,1]
-    end = [2020,1,31]
+    num_years = 10
     location_coords = [-32.8960021, 151.734908] # for mayfield east
-    elevation = 10
+    elevation = 100
+
+    start = [2021 - num_years, 1, 1]
+    end = [2021, 1, 1]
 
     data = historical_daily_temperatures(start, end, location_coords, elevation)
-    data.plot(y=['tavg', 'tmin', 'tmax'])
+
+    plt.hist(data.tmin, bins=30)
+    plt.hist(data.tavg, bins=30)
+    plt.hist(data.tmax, bins=30)
     plt.show()
 
-    tavg, tmin, tmax = typical_months_temperatures(1, location_coords, elevation)
-    plt.plot(tavg)
-    plt.plot(tmin)
-    plt.plot(tmax)
-    plt.show()
+    min_temp, max_temp, avg_temp = location_design_temp(location_coords, elevation, num_years=10, percentiles=[1, 99])
 
-    ymd = [2022, 1, 20]
-    sunrise, sunset, noon = get_sun_times(location_coords, ymd)
-
+    print('Design day min temp for mayfield = ', min_temp)
+    print('Design day max temp for mayfield = ', max_temp)
+    print('Design day average temp for mayfield = ', avg_temp)
 
