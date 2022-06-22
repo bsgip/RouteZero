42c67e5d8a86a0012ad8340dd6964f94ab94db8e
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2022-06-22 12:16:45 +1000

    first attempt at pagse

---
+++ pages/about.py
@@ -0,0 +1,13 @@
+import dash
+from dash import html, dcc
+
+dash.register_page(__name__)
+
+layout = html.Div(children=[
+    html.H1(children='About RouteZero'),
+
+    html.Div(children='''
+        About contents.
+    '''),
+
+])
