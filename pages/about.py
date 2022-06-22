d4a6161cd2f6d8b518ce47356c264bbc8f96a227
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2022-06-22 13:15:05 +1000

    tidy

--- pages/about.py
+++ pages/about.py
@@ -9,11 +9,4 @@ layout = html.Div(children=[
     ## About
     Documentation to come
     """)
-    # dbc.Col(html.H1(children='About RouteZero'), align='center'),
-    # html.H1(children='About RouteZero'),
-    #
-    # html.Div(children='''
-    #     About contents.
-    # '''),
-
 ], style={'margin-left': "18rem"})
